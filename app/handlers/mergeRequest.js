f2901c9c0d9291255e3d155fc689d464dfab2d62
Monica Mota Ocariz <monica.ocariz@i-value.com.br>
2021-11-18 11:26:51 -0300

    :recycle: Refactor mergeRequest handle to display only open and reopen merge request

--- app/handlers/mergeRequest.js
+++ app/handlers/mergeRequest.js
@@ -27,62 +27,44 @@ module.exports = async function (data) {
 		return;
 	}
 
-	const verb = helpers.actionToVerb(mr.action),
-		description = mr.description.split(/(?:\r\n|[\r\n])/)[0], // Take only the first line of the description.
-		/* eslint-disable camelcase */ // Required property naming.
-		attachment = {
-			mrkdwn_in: ['text'],
-			color: module.exports.COLOR,
-			title: mr.title,
-			title_link: mr.url
-		};
-		/* eslint-enable camelcase */
-
-	let assigneeName = '_none_';
-
-	if (data.assignee) {
-		assigneeName = util.format('<%s/u/%s|%s>', config.gitLab.baseUrl, data.assignee.username, data.assignee.username);
-	}
-
-	const output = {
-		parse: 'none',
-		text: util.format(
-			'[%s] <%s/u/%s|%s> %s merge request *!%s* — *source:* <%s/tree/%s|%s> — *target:* <%s/tree/%s|%s> - *assignee* - %s',
-			data.project.path_with_namespace,
-			config.gitLab.baseUrl,
-			data.user.username,
-			data.user.username,
-			verb,
-			mr.iid,
-			mr.source.web_url,
-			mr.source_branch,
-			mr.source_branch,
-			mr.target.web_url,
-			mr.target_branch,
-			mr.target_branch,
-			assigneeName
-		),
-		attachments: [attachment]
+	const description = mr.description.split(/(?:\r\n|[\r\n])/)[0]; // Take only the first line of the description.
+	const attachment = {
+		mrkdwn_in: ['text'],
+		color: (data.labels[0] || {}).color || module.exports.COLOR,
+		title: mr.title,
+		title_link: mr.url
 	};
 
-	// Start the fallback with the title.
-	attachment.fallback = attachment.title;
+	if (mr.action === 'open' || mr.action === 'reopen') {
+		const output = {
+			parse: 'none',
+			text: util.format(
+				'@aqui <%s/%s|%s> Abriu um Merge Request *!%s* — *source:* <%s/tree/%s|%s> — *target:* <%s/tree/%s|%s>',
+				config.gitLab.baseUrl,
+				data.user.username,
+				data.user.username,
+				mr.iid,
+				mr.source.web_url,
+				mr.source_branch,
+				mr.source_branch,
+				mr.target.web_url,
+				mr.target_branch,
+				mr.target_branch
+			),
+			attachments: [attachment]
+		};
 
-	switch (mr.action) {
-		case 'open':
-		case 'reopen':
-			// Open and re-open are the only ones that get the full merge request description.
-			attachment.fallback += '\n' + mr.description;
-			attachment.text = helpers.convertMarkdownToSlack(description, mr.source.web_url);
+		// Start the fallback with the title.
+		attachment.fallback = attachment.title;
+		attachment.fallback += '\n' + mr.description;
+		attachment.text = helpers.convertMarkdownToSlack(description, mr.source.web_url);
 
-			break;
+		debug('Message handled.');
+
+		output.__kind = module.exports.KIND;
+
+		return output;
 	}
-
-	debug('Message handled.');
-
-	output.__kind = module.exports.KIND;
-
-	return output;
 };
 
 /**
